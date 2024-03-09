2ff2865f548ddf4ca44a99af778c2ef59aa8be7e
kaushalsatam <kaushalabd17@gmail.com>
2024-03-09 20:09:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("input")[0].addEventListener("click", function (){
+    this.removeAttribute("placeholder");
+});
+
+document.querySelectorAll("input")[1].addEventListener("click", function (){
+    this.removeAttribute("placeholder");
+});
